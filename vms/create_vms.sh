e5ea8d253ee7e456bcbc812d81912da9778ecef6
mathieugery <mathieu.gery@epitech.eu>
2020-10-05 14:46:02 +0200

    Add bridged adaptaer

--- vms/create_vms.sh
+++ vms/create_vms.sh
@@ -1,14 +1,14 @@
 #Create the virtual machine
 
-VBoxManage createvm --name machine1 --ostype Debian_64 --register
+VBoxManage createvm --name $1 --ostype Debian_64 --register
 
 
 #Edit the vm
 
-VBoxManage modifyvm machine1 --memory 512 
-VBoxManage modifyvm machine1 --nic1 nat
-VBoxManage modifyvm machine1 --nictype1 virtio 
-VBoxManage modifyvm machine1 --boot1 net
+VBoxManage modifyvm $1 --memory 512
+VBoxManage modifyvm $1 --nic1 bridged
+VBoxManage modifyvm $1 --nictype1 virtio 
+VBoxManage modifyvm $1 --boot1 net
 
 #Start the vm
 
