8976c264f7a0d0ac22322c185afb6df298c1d1ca
mathieugery <mathieu.gery@epitech.eu>
2020-10-05 14:59:29 +0200

    Modify createvm.sh to handle vars gesture

--- vms/create_vms.sh
+++ vms/create_vms.sh
@@ -2,14 +2,13 @@
 
 VBoxManage createvm --name $1 --ostype Debian_64 --register
 
-
 #Edit the vm
 
 VBoxManage modifyvm $1 --memory 512
-VBoxManage modifyvm $1 --nic1 bridged
+VBoxManage modifyvm $1 --nic1 bridged --nictype1 82545EM --bridgeadapter1 en0
 VBoxManage modifyvm $1 --nictype1 virtio 
 VBoxManage modifyvm $1 --boot1 net
 
 #Start the vm
 
-VBoxHeadless --startvm machine1
+VBoxHeadless --startvm $1
